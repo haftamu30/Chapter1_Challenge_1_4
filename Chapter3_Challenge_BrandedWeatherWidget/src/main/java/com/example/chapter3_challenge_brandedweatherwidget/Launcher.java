d9de7c8cc7b86600da1ca61844a5996bbe71eb84
Haftamu <haftamum239@gmail.com>
2026-01-04 00:07:52 +0300

    Delete Chapter3_Challenge_BrandedWeatherWidget directory

--- Chapter3_Challenge_BrandedWeatherWidget/src/main/java/com/example/chapter3_challenge_brandedweatherwidget/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.example.chapter3_challenge_brandedweatherwidget;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
